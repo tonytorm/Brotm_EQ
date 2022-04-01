586f14f88b269c88fa5de8143cac61d5d92619a3
tonytorm <giulio2.iacomino@live.uwe.ac.uk>
2022-04-01 23:48:42 +0100

    bettered sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -55,6 +55,12 @@ void RotarySliderWithLabels::paint(juce::Graphics &g)
     
     auto sliderBounds = getSliderBounds();
     
+    g.setColour(Colours::red);
+    g.drawRect(getLocalBounds());
+    
+    g.setColour(Colours::yellow);
+    g.drawRect(sliderBounds);
+    
     getLookAndFeel().drawRotarySlider(g, sliderBounds.getX(),
                                       sliderBounds.getY(),
                                       sliderBounds.getWidth(),
@@ -65,7 +71,19 @@ void RotarySliderWithLabels::paint(juce::Graphics &g)
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
 {
-    return getLocalBounds();
+    auto bounds = getLocalBounds();
+    
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+    
+    size -= getTextHeight() * 2;
+    
+    juce::Rectangle<int> r;
+    
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+    
+    return r;
 }
 //===================================================================================================================================
 ResponseCurveComponent::ResponseCurveComponent(Brotm_EQAudioProcessor& p) : audioProcessor(p)
